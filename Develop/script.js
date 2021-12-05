32b1e4951dfefafc55e5bf3acee439b6fb6db9b2
Chris Fraser <chrisfraser@Chriss-MBP.home>
2021-12-05 16:20:46 -0500

    almost done

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //causes current day and time to display
     $("#currentDay").text(moment().format("MMMM do YYYY, h:mm:ss a"));
     $(".saveBtn").on("click", function () {
         console.log(this);
@@ -6,10 +7,19 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
-    $("#hour9 .description").val(localStorage.getItem("hour9"));  
+    //data for each hour saved to LocalStorage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10")); 
+    $("#hour11 .description").val(localStorage.getItem("hour11")); 
+    $("#hour12 .description").val(localStorage.getItem("hour12")); 
+    $("#hour13 .description").val(localStorage.getItem("hour13")); 
+    $("#hour14 .description").val(localStorage.getItem("hour14")); 
+    $("#hour15 .description").val(localStorage.getItem("hour15")); 
+    $("#hour16 .description").val(localStorage.getItem("hour16")); 
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));    
 
-
-
+    //function that tracks the hour
     function hourTracker() {
         var currentHour = moment().hour();
 
